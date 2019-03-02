b62958d2ef9ca3512dba23bbd494c250ebae3234
Sean McConnell <47701446+Spmccon617@users.noreply.github.com>
2019-03-01 19:21:20 -0500

    Data types

---
+++ datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int age = 40; /* data type for integers */
+
+	double gpa = 3.7; /* data type for decimals, holds up to 15 decimal places */
+	float gpa = 4.9; /* data type for decimals, holds up to 6 decimal places */
+
+	char grade = 'A'; /* holds character */
+	char phrase [] = "I love C"; /* holds strings */
+}
